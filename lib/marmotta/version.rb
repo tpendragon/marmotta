2eb636ddc010692ab8e4da0f7f0f3d53b7837676
Trey Terrell <trey.terrell@oregonstate.edu>
2015-07-12 04:17:05 -0700

    Increment header.

--- lib/marmotta/version.rb
+++ lib/marmotta/version.rb
@@ -1,3 +1,3 @@
 module Marmotta
-  VERSION = "0.2.0"
+  VERSION = "0.2.1"
 end
--- spec/marmotta/ld_path_connection_spec.rb
+++ spec/marmotta/ld_path_connection_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Marmotta::LdPathConnection do
     it "should return a JSON of the result" do
       result = {"test" => 1}
       stub_request(:get, "http://localhost:8983/marmotta/ldpath/path?path=%3Chttp://www.w3.org/2004/02/skos/core%23altLabel%3E&uri=http://bla.bla.org").
-        with(:headers => {'Accept'=>'application/ld+json', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Content-Type'=>'application/ld+json', 'User-Agent'=>'Hurley v0.1'}).
         to_return(:status => 200, :body => result.to_json, :headers => {})
 
       response = subject.get("http://bla.bla.org")
